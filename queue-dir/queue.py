27ba005518788c53fd1acefa227b61afda216b38
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-04-02 13:47:43 -0700

    update for git pull

--- queue-dir/queue.py
+++ queue-dir/queue.py
@@ -19,50 +19,89 @@ class Queue:
         """
         returns current number of nodes in queue
         """
-        return self._size        
+        return self._size
+
+    # def__repr__(self):
+    #     """
+    #     prints out something helpful for dev
+    #     """
+
+    def __str__(self):
+        """
+        prints out self._back
+        """
+        return 'Queue back: {}'.format(self._back.val)
 
     def enqueue(self, val):
         """
         method which adds a value to the back of the queue
         """
-        self._back = Node(val, self._back)
+        ## Megan flipped method
+        node = Node(val)
+        
+        if self._size == 0
+            self._front = self._back = node
+            self._size += 1
+            return node
+
+        self._back._next = self._back = node
         self._size += 1
-        return self._back
+        return node
+
+        ## Scott
+        # if self._front is None:
+        #     self._front = self._back = Node(val)
+        #     return self._back
+        # self._size += 1
+        # self._back = Node(val, self._back)
+        # return self._back
+
+        ## Tyler Original
+        # self._back = Node(val, self._back)
+        # self._size += 1
+        # return self._back
 
     def dequeue(self):
         """
         iterates through the queue to find front node, then removes/returns it
         """
+        ## Megan flipped Method
+        temp = self._front
+        self._front = temp._next
+        self._size -= 1
+        return temp
+
+        ## Tyler method
         ## need to test for 0 and 1 node
-        if self._size == 0:
-            print('Queue is empty, nothing to dequeue')
-            return False
-        elif self._size == 1:
-            current = self._back
-            self._back = None
-            self._size -= 1
-            return current
-        else:
-            current = self._back
-            while current._next is not None:
-                prev = current
-                current = current._next
-            prev._next = None
-            self._size -= 1
-            return current
+        # if self._size == 0:
+        #     print('Queue is empty, nothing to dequeue')
+        #     return False
+        # elif self._size == 1:
+        #     current = self._back
+        #     self._back = None
+        #     self._size -= 1
+        #     return current
+        # else:
+        #     current = self._back
+        #     while current._next is not None:
+        #         prev = current
+        #         current = current._next
+        #     prev._next = None
+        #     self._size -= 1
+        #     return current
 
-one_four_queue = Queue([1, 2, 3, 4, 'a'])
-print(one_four_queue._size)
-one_four_queue.enqueue(0)
-one_four_queue.dequeue()
-one_four_queue.dequeue()
-one_four_queue.dequeue()
-# test if it tries to dequeue when queue is empty
-current = one_four_queue._back
-print(one_four_queue._size)
+# one_four_queue = Queue([1, 2, 3, 4, 'a'])
+# print(one_four_queue._size)
+# one_four_queue.enqueue(0)
+# one_four_queue.dequeue()
+# one_four_queue.dequeue()
+# one_four_queue.dequeue()
+# # test if it tries to dequeue when queue is empty
+# current = one_four_queue._back
+# print(one_four_queue._size)
 
-while current is not None:
-    print(current.val)
-    current = current._next
+# while current is not None:
+#     print(current.val)
+#     current = current._next
 
-print(one_four_queue.__len__)
+# print(one_four_queue.__len__)
