22e8c1d824014703a9147ada062a981827d733d8
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-04-04 18:11:40 -0700

    whiteboard complete with tests

---
+++ multi-bracket-validation/README.md
@@ -0,0 +1,9 @@
+# Overview
+I was working is the lab late on night, when my eyes beheld an erie sight. A monster with these word tattooed across its chest was standing before me:
+
+## Challenge
+Make a function that takes a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced. There are 3 types of brackets:
+Round Brackets : ()
+Square Brackets : []
+Curly Brackets : {}
+
---
+++ multi-bracket-validation/stack.py
@@ -0,0 +1,75 @@
+from node import Node
+
+class Stack():
+    """
+    creating a Stack class for use in data structures
+    """
+    def __init__(self, iter = []):
+        """
+        initiatilizes the stack with default properties unless specific
+        """
+        self.top = None
+        self._size = 0
+
+        for i in reversed(iter):
+            self.push(i)
+               
+    def __len__(self):
+        """
+        returns length of stack
+        """
+        return self._size
+    
+    def __str__(self):
+        """
+        returns a formatted string for info about stack
+        """
+        return 'This stack is {} nodes'.format(self._size)
+    
+    def push(self, val):
+        """
+        places a new node on top of the stack and sets it as the new top
+        """
+        node = Node(val)
+        node._next = self.top
+        self.top = node
+        # self.top = Node(val, self.top) <- is dryer way to do this
+        self._size += 1
+        return self.top
+    
+    def pop(self):
+        """
+        removes the top node on the array and returns it
+        """
+        if self.top is not None:
+            node_to_remove = self.top
+            self.top = self.top._next
+            self._size -= 1
+            return node_to_remove
+        else:
+            print('Cannot pop from empty stack')
+            return False
+    
+    def peek(self):
+        """
+        returns the node at the top of the stack
+        """
+        if self.top is not None:
+            return self.top
+        else:
+            return False
+            
+
+# one_four_stack = Stack([1, 2, 3, 4, 'a'])
+# one_four_stack.push(0)
+# one_four_stack.pop()
+# one_four_stack.pop()
+# one_four_stack.pop()
+# # test if it tries to pop when stack is empty
+# current = one_four_stack.top
+
+# while current is not None:
+#     print(current.val)
+#     current = current._next
+
+# print(one_four_stack.peek())
