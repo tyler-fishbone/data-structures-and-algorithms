74b7a7f8d68f348e30126341a41d6daa21a185f3
Tyler Fishbone <tyler.fishbone@gmail.com>
2018-06-01 14:42:17 -0700

    removed some corpose code from the bottom of breadth_first_traversal

--- challenges/breadth-first-traversal/breadth_first_traversal.py
+++ challenges/breadth-first-traversal/breadth_first_traversal.py
@@ -3,7 +3,7 @@ from queue import Queue
 from bst import BST
 
 def breadth_first_traversal(binary_search_tree):
-    """ function for traversing through bst 
+    """ function for traversing through bst
     in left to right breadth first order """
 
     array_for_testing = []
@@ -16,24 +16,20 @@ def breadth_first_traversal(binary_search_tree):
         try:
             if current_bst.left is not None:
                 q.enqueue(current_bst.left)
-            
+
             if current_bst.right is not None:
                 q.enqueue(current_bst.right)
         except AttributeError:
             print('Cannot traverse empty tree')
             return False
-        
+
         print(q._front.val.val)
         array_for_testing.append(q._front.val.val)
-        
+
         q.dequeue()
         if q._size <= 0:
             break
         else:
             current_bst = q._front.val
-            
+
     return array_for_testing
-
-
-# lil_bst = BST([5,3,1,4,10,7,12])
-# breadth_first_traversal(lil_bst)
